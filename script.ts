ba6a442dc7b8cabf032f5bf36c6658d9e113ade9
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-12-11 16:37:06 +0100

    do the 3 first exo in script.ts

--- script.ts
+++ script.ts
@@ -4,7 +4,26 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+    // ... you will write your Prisma Client queries here
+    let result = await prisma.user.findMany();
+    console.log('1 - ', result);
+    await prisma.user.create({
+        data: {
+            email: 'test@gmail.com'
+        }
+    });
+    result = await prisma.user.findMany();
+    console.log('2 - ', result);
+    await prisma.user.update({
+        where: {
+            email: 'test@gmail.com'
+        },
+        data: {
+            name: 'test'
+        }
+    });
+    result = await prisma.user.findMany();
+    console.log('3 - ', result);
 }
 
 main()

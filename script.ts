7983db5b2a5ba6c59cc8d6dcbfa0479bd7307371
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-12-11 17:11:36 +0100

    do the 4-5-6 exos in script.ts

--- script.ts
+++ script.ts
@@ -4,26 +4,46 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-    // ... you will write your Prisma Client queries here
-    let result = await prisma.user.findMany();
-    console.log('1 - ', result);
-    await prisma.user.create({
+
+    const email: string = 'test@gmail.com';
+
+    const result1 = await prisma.user.findMany();
+    console.log('1 - ', result1);
+    const result2 = await prisma.user.create({
         data: {
-            email: 'test@gmail.com'
+            email: email
         }
     });
-    result = await prisma.user.findMany();
-    console.log('2 - ', result);
-    await prisma.user.update({
+    console.log('2 - ', result2);
+    const result3 = await prisma.user.update({
         where: {
-            email: 'test@gmail.com'
+            email: email
         },
         data: {
             name: 'test'
         }
     });
-    result = await prisma.user.findMany();
-    console.log('3 - ', result);
+    console.log('3 - ', result3);
+    console.log('4 - the schema of prisma was edited (add posts)');
+    const result5 = await prisma.post.create({
+        data: {
+            title: "Hello world"
+        }
+    });
+    console.log('5 - ', result5)
+    const result6 = await prisma.post.update({
+        where: {
+          id: 1
+        },
+        data: {
+            author: {
+                connect: {
+                    email: email
+                }
+            }
+        }
+    });
+    console.log('6 - ', result6)
 }
 
 main()
